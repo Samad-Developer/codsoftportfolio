7e37c9bbe28f803911b0d1ce65a3d603aac5bbe2
Abdus samad <abdussamadkotha@gmail.com>
2024-04-19 18:49:14 +0500

    first commit

--- components/email.jsx
+++ components/email.jsx
@@ -86,7 +86,7 @@ const email = () => {
         </div>
         <button
           type="submit"
-          className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-[#1F1F1F]"
         >
           Send Message
         </button>
